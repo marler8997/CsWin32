da4c22b73d5f94b6cc3eeb29b21d8db2705edfc4
Jonathan Marler <johnnymarler@gmail.com>
2021-01-23 17:14:55 -0700

    Combine Generator Library with Exe, add solution file

--- zig/GlobalSuppressions.cs
+++ zig/GlobalSuppressions.cs
@@ -10,3 +10,18 @@ using System.Diagnostics.CodeAnalysis;
     "CA1303:Do not pass literals as localized parameters",
     Justification = "This project only supports English",
     Scope = "module")]
+[assembly: SuppressMessage(
+    "StyleCop.CSharp.NamingRules",
+    "SA1310:Field names should not contain underscore",
+    Justification = "This code will be ported to Zig",
+    Scope = "module")]
+[assembly: SuppressMessage(
+    "Naming",
+    "CA1707:Identifiers should not contain underscores",
+    Justification = "This code will be ported to Zig",
+    Scope = "module")]
+[assembly: SuppressMessage(
+    "StyleCop.CSharp.DocumentationRules",
+    "SA1600:Elements should be documented",
+    Justification = "This code is not meant to be used as a public library",
+    Scope = "module")]
--- zig/ZigWin32/AnalyzerReleases.Unshipped.md
+++
@@ -1,9 +0,0 @@
-﻿; Unshipped analyzer release
-; https://github.com/dotnet/roslyn-analyzers/blob/master/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
-
-### New Rules
-Rule ID | Category | Severity | Notes
---------|----------|----------|-------
-PInvoke001 | Functionality | Warning | SourceGenerator
-PInvoke002 | Functionality | Warning | SourceGenerator
-PInvoke003 | Functionality | Warning | SourceGenerator
